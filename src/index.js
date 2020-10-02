58e48744b2a574e0e1b8e5855eb3da17f433b750
YuPashintseva <juliyapashintseva@gmail.com>
2020-10-02 15:15:32 +0600

    fix: change second arr

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+	if (matrix) {
+	  var finalArray = [];
+	  for(var i=0; i<matrix.length; i+=1) {
+	    if ((i+1)%2 === 0) {
+	     for (var j=matrix[i].length-1; j>=0; j-=1) {
+	          finalArray.push(matrix[i][j]);
+	      }
+	    } else {
+	      for (var j=0; j<matrix[i].length; j+=1) {
+	          finalArray.push(matrix[i][j]);
+	      }
+	    } 
+	   
+	  }
+	  return finalArray;
+	}
+
+	return [];
+  }
